b0b998ff818bca693b97bb05701a39e2b6f78941
ICOM STUDENTS <yetsiel.aviles@upr.edu>
2018-04-27 10:56:59 -0400

    First

---
+++ src/labUtils/Utils.java
@@ -0,0 +1,38 @@
+package labUtils;
+
+import treeClasses.LinkedBinaryTree;
+import treeClasses.LinkedTree;
+import treeInterfaces.Position;
+import treeInterfaces.Tree;
+
+public class Utils {
+	public static <E> void displayTree(String msg, Tree<E> t) { 
+		System.out.println("\n\n" + msg); 
+		t.display(); 
+	}
+
+	public static <E> void displayTreeElements(String msg, Tree<E> t) {
+		System.out.println("\n\n" + msg); 
+		for (E element : t)
+			System.out.println(element); 
+		
+	}
+	
+	public static LinkedTree<Integer> buildExampleTreeAsLinkedTree() { 
+		LinkedTree<Integer> t = new LinkedTree<>(); 
+		// ADD CODE AS SPECIFIED IN EXERCISE 2
+		
+		
+		return t; 
+	}
+	
+	public static LinkedBinaryTree<Integer> buildExampleTreeAsLinkedBinaryTree() { 
+		LinkedBinaryTree<Integer> t = new LinkedBinaryTree<>(); 
+		// ADD CODE AS SPECIFIED IN EXERCISE 2
+		
+		
+		return t; 
+	}
+
+
+}
